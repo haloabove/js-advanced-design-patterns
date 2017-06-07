0a599372be5eea488c0f7915a90ceae67d2d9b19
Tihomir Jovchevski <tihomir.jovchevski@internetbrands.com>
2017-06-07 16:00:23 +0200

    added a module pattern

---
+++ module.js
@@ -0,0 +1,27 @@
+var ModulePattern = (function(option) {
+	this.options = $.extend({
+		optionOne	: '',
+		optionTwo	: 0,
+		contents	: 'Content'
+	},option);
+
+  	// declare private variables and/or functions
+	var contents = this.options.contents;
+	
+  	var changeHTML = function() {
+	    var element = document.getElementById('attribute-to-change');
+	    	element.innerHTML = contents;
+  	};
+
+	return {
+		// declare public variables and/or functions
+		callChangeHTML: function() {
+		  changeHTML();
+		  console.log(contents);
+		}
+	};
+
+})();
+
+ModulePattern.callChangeHTML();       // Outputs: 'contents'
+console.log(HTMLChanger.contents);  // undefined
